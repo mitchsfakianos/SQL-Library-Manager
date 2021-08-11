e1abc33746f7cee0cfeb3bee5dd61f8c5837c13b
mazarinno <mcsfakianos@gmail.com>
2021-08-11 17:04:42 -0500

    views completed, setting up routes

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,26 @@ var express = require('express');
 var router = express.Router();
 const Book = require('../models').Book;
 
+router.get('/books/new', (req, res, next) => {
+	res.render('new-book', { book: Book.build(), title: 'New Book'});
+});
+
+router.post('/books/new', (req, res) => {
+
+});
+
+router.get('/books/:id', (req, res, next) => {
+
+});
+
+router.post('/books/:id', (req, res) => {
+
+});
+
+router.post('/books/:id/delete', (req, res) => {
+
+});
+
 router.get('/books', (req, res) => {
 	Book.findAll().then( function(books) {
 		var list = { books: books };
